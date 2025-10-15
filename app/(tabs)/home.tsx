3fd1acfa5c12feafeadc1dcd466cb0711507f28d
gerrra <bogzaschita@gmail.com>
2025-10-14 21:55:38 -0700

    feat: configure path aliases with @/ for TypeScript, Babel and ESLint

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Screen>
       <Text style={{ fontSize: 24, fontWeight: '600' }}>Home</Text>
-      <Text>Добро пожаловать! Это стартовый экран.</Text>
+      <Text>Добро пожаловать! Это стартовый экран. Тест 1</Text>
     </Screen>
   );
 }
